26f3384b8b4e905c75c21bf7e763ab4d46faf624
이지윤 <1yoouoo@gmail.com>
2022-10-29 16:23:13 +0900

    22-10-29 wip2

---
+++ React_netflix/src/Main.js
@@ -0,0 +1,11 @@
+import MainHeader from "./Component/MainHeader"
+
+const Main = () => {
+
+
+    return (
+        <MainHeader />
+    )
+}
+
+export default Main
--- React_netflix/src/Routing.js
+++ React_netflix/src/Routing.js
@@ -3,6 +3,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import App from './App';
 import Login from './Component/Login';
+import Main from './Main';
 
 
 const Routing = () => {
@@ -12,6 +13,7 @@ const Routing = () => {
                 <Route exact path="/*" element={<App />} />
                 <Route exact path="/app" element={<App />} />
                 <Route exact path="/Login" element={<Login />} />
+                <Route exact path="/Main" element={<Main />} />
             </Routes>
         </>
     );

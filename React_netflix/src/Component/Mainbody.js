b6bb02742a9aaaa0cbfe92a5f2cb92a3978e6459
이지윤 <1yoouoo@gmail.com>
2022-10-31 21:25:38 +0900

    22-10-31 wip

--- React_netflix/src/Component/Mainbody.js
+++ React_netflix/src/Component/Mainbody.js
@@ -2,22 +2,54 @@ import "./Mainbody.css"
 import Slider from "./Slider"
 
 const Mainbody = () => {
-    const imgList = ["1", "2", "3"]
+    let imgList = [
+        { 
+            url : require('./img/sliderImgitem1.webp'),
+            caption : 'img 1'
+        },
+        { 
+            url : require('./img/sliderImgitem2.webp'),
+            caption : 'img 2'
+        },
+        { 
+            url : require('./img/sliderImgitem3.webp'),
+            caption : 'img 3'
+        },
+
+    ]
+
+
+    // build(실행준비)  
+        // <Slider 
+        // title = {"한국이 만든 콘텐츠"}
+        // imgList = {<img src={require('./img/sliderImgitem1.webp')} alt="asd" />}
+    // runtime(실행)
+        // 해봐야지 안다.
+
 
     return (
         <div className="mainview-container">
             <div className="mainview">
-                <span>
+                <span className="main" >
                     <img className="main-img" src={require('./img/Netflix-Main-img.webp')} alt="Main Img"/>
+                    <div>asd</div>
+                    <div>ss</div>
+                    <div>dd</div>
                 </span>
                 <div>
                     <Slider 
-                        title = {"한국이 만든 콘텐츠"}
-                        imgList = {imgList}
-                    />
-                    <Slider 
                         title = {"미국이 만든 콘텐츠"}
-                        imgList = {imgList}
+                        imgList = {imgList.map((img)=> {
+                            return (
+                                    <div className="each-slide" key={img.caption}>
+                                        <div>
+                                            <img src={img.url} alt="asd"/>
+                                        </div>
+                                    </div>
+                                    )
+                                }
+                            )
+                        }
                     />
                 </div>
             </div>

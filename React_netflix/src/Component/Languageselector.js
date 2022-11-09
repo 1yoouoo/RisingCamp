dd27ea9b7d6a4558e2813a42d401ce35e3b7f8c3
이지윤 <1yoouoo@gmail.com>
2022-11-09 16:57:24 +0900

    Design: Language Selector 에 fontAwsome 적용ㅇ

--- React_netflix/src/Component/Languageselector.js
+++ React_netflix/src/Component/Languageselector.js
@@ -1,4 +1,6 @@
 import "./Languageselector.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown, faGlobe } from '@fortawesome/free-solid-svg-icons'
 
 const Languageselector = () => {
     return (
@@ -8,11 +10,14 @@ const Languageselector = () => {
                     <span className="ui-label-text">Select Language</span>
                 </label>
                 <div className="select-arrow medium prefix globe">
+
+                    <FontAwesomeIcon icon={faGlobe} size={'xs'} className="faGlobe"/>
                     <select className="ui-select medium" id="lang-switcher-select" tabIndex="0">
                         <option lang="ko" defaultValue="/kr/" >한국어
                         </option>
                         <option lang="en" defaultValue="/kr-en/">English</option>
                     </select>
+                    <FontAwesomeIcon icon={faCaretDown} className="faCaretDown"/>
                 </div>
             </div>
         </div>

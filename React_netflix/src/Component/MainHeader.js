d0869c2170a8257353b26754cc2bfd341afcd8f3
이지윤 <1yoouoo@gmail.com>
2022-11-09 13:53:34 +0900

    Feat: 검색 버튼 누르면 인풋창 나옴

--- React_netflix/src/Component/MainHeader.js
+++ React_netflix/src/Component/MainHeader.js
@@ -9,6 +9,11 @@ const MainHeader = () => {
         window.addEventListener('scroll', updateScroll);
     });
 
+    const [buttonOnClick, setButtonOnClick] = useState(false);
+    const handleToggle = () => {
+            setButtonOnClick(!buttonOnClick);
+        };
+
     return (
         <div className={scrollPosition < 50 ? "pinning-header" : "moving-header"}>
             <div className="pinning-header-container" style={{top: "0px", position: "relative", background: "transparent"}}>
@@ -44,17 +49,47 @@ const MainHeader = () => {
                         </li>   
                     </ul>
 
-                    <div className="secondary-navigation">
-                        <div className="nav-element">
-                            <div className="searchBox">
-                                <button className="searchTab" tabIndex="0" aria-label="검색" data-uia="search-box-launcher">
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="search-icon">
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M13 11C13 13.7614 10.7614 16 8 16C5.23858 16 3 13.7614 3 11C3 8.23858 5.23858 6 8 6C10.7614 6 13 8.23858 13 11ZM14.0425 16.2431C12.5758 17.932 10.4126 19 8 19C3.58172 19 0 15.4183 0 11C0 6.58172 3.58172 3 8 3C12.4183 3 16 6.58172 16 11C16 11.9287 15.8417 12.8205 15.5507 13.6497L24.2533 18.7028L22.7468 21.2972L14.0425 16.2431Z" fill="currentColor">
-                                        </path>
-                                    </svg>
-                                </button>
+                    <div className={buttonOnClick ? "secondary-navigation-searchFocused" : "secondary-navigation"}>
+                        {buttonOnClick ? (
+                            <div className="nav-element">
+                                <div className="searchBox">
+                                    <button className="searchTab" onClick={handleToggle}>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="search-icon">
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M13 11C13 13.7614 10.7614 16 8 16C5.23858 16 3 13.7614 3 11C3 8.23858 5.23858 6 8 6C10.7614 6 13 8.23858 13 11ZM14.0425 16.2431C12.5758 17.932 10.4126 19 8 19C3.58172 19 0 15.4183 0 11C0 6.58172 3.58172 3 8 3C12.4183 3 16 6.58172 16 11C16 11.9287 15.8417 12.8205 15.5507 13.6497L24.2533 18.7028L22.7468 21.2972L14.0425 16.2431Z" fill="currentColor">
+                                            </path>
+                                        </svg>
+                                    </button>
+                                    <input className="searchInput" placeholder="제목, 사람, 장르" />
+                                </div>
                             </div>
-                        </div>
+                        ) : (
+                                <div className="nav-element">
+                                    <div className="searchBox">
+                                        <button className="searchTab" onClick={handleToggle}>
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="search-icon">
+                                                <path fillRule="evenodd" clipRule="evenodd" d="M13 11C13 13.7614 10.7614 16 8 16C5.23858 16 3 13.7614 3 11C3 8.23858 5.23858 6 8 6C10.7614 6 13 8.23858 13 11ZM14.0425 16.2431C12.5758 17.932 10.4126 19 8 19C3.58172 19 0 15.4183 0 11C0 6.58172 3.58172 3 8 3C12.4183 3 16 6.58172 16 11C16 11.9287 15.8417 12.8205 15.5507 13.6497L24.2533 18.7028L22.7468 21.2972L14.0425 16.2431Z" fill="currentColor">
+                                                </path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            )
+                        }
+
+{/* 
+    <div className="nav-element">
+        <div className="searchBox">
+            <button className="searchTab">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="search-icon">
+                    <path fillRule="evenodd" clipRule="evenodd" d="M13 11C13 13.7614 10.7614 16 8 16C5.23858 16 3 13.7614 3 11C3 8.23858 5.23858 6 8 6C10.7614 6 13 8.23858 13 11ZM14.0425 16.2431C12.5758 17.932 10.4126 19 8 19C3.58172 19 0 15.4183 0 11C0 6.58172 3.58172 3 8 3C12.4183 3 16 6.58172 16 11C16 11.9287 15.8417 12.8205 15.5507 13.6497L24.2533 18.7028L22.7468 21.2972L14.0425 16.2431Z" fill="currentColor">
+                    </path>
+                </svg>
+            </button>
+            <input className="searchInput" placeholder="제목, 사람, 장르" />
+        </div>
+    </div> */}
+
+                        
                         <div className="nav-element show-kids">
                             <a href="/Kids">키즈</a>
                         </div>

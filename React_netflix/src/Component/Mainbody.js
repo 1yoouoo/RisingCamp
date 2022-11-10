6361f344c2b267793579c9b16a8d6d60313734ff
이지윤 <1yoouoo@gmail.com>
2022-11-10 17:03:50 +0900

    Design: mainImgInfo

--- React_netflix/src/Component/Mainbody.js
+++ React_netflix/src/Component/Mainbody.js
@@ -4,6 +4,8 @@ import SliderImg from "./SliderImg";
 import Modal from "./Modal";
 import React, { useState } from 'react';
 import API from './../Api/Api';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 
 
 const Mainbody = () => {
@@ -22,10 +24,37 @@ const Mainbody = () => {
         <div className="mainview-container">
             <div className="mainview">
                 <Modal artId={myArtId} open={modalOpen} close={closeModal}/>
-                <span className="main" >
-                    <img className="main-img" src={require('./img/Netflix-Main-img.webp')} alt="Main Img"/>
+                <div className="main" >
+                    <img className="main-img" src={require('./img/Netflix-Main-img.jpeg')} alt="Main Img"/>
                     <span className="main-img-gradient"></span>
-                </span>
+                    <div className="main-img-info-wrapper">
+                        <div className="main-img-info">
+                            <div className="main-img-title">
+                                <img src={require('./img/Netflix-Main-img-title.png')} alt=""/>
+                            </div>
+                            <div className="main-img-subtitle">
+                                <div>지금 시청하세요</div>
+                                <div>
+                                    시카고의 잘나가는 재무 컨설턴트 마티는 가족과 함께 미주리 주 오자크로 이사한다. 목적은 하나, 파트너가 횡령한 마약 조직의 돈 5억 달러를 돈세탁하는 것.
+                                </div>
+                            </div>
+                            <div className="main-img-info-button-wrapper">
+                                <a href="/">
+                                    <button>
+                                        <FontAwesomeIcon icon={faPlay} className="faPlay" />
+                                        <span>재생</span>
+                                    </button>
+                                </a>
+                                <a href="/">
+                                    <button>
+                                        <FontAwesomeIcon icon={faCircleInfo} className="faCircleInfo"/>
+                                        <span>상세 정보</span>
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="slider-wrapper" onClick={openModal}>
                     {datas.recommenedGenreList.map(( genre ) => {
                         return (

32f31324223139cb99a574f668f8367f0a5d83f7
이지윤 <1yoouoo@gmail.com>
2022-11-12 16:01:37 +0900

    Design: add info in modal

--- React_netflix/src/Component/Modal.js
+++ React_netflix/src/Component/Modal.js
@@ -3,7 +3,7 @@ import "./Modal.css"
 import API from './../Api/Api';
 import ModalInfo from './ModalInfo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay, faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faPlus, faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
 
 //mouse over leave
 const Modal = (props) => {
@@ -27,29 +27,32 @@ const Modal = (props) => {
                         <video autoPlay playsInline muted>
                             <source src={require(`${'./img/video/main-video.mov'}`)} type="video/mp4"/>
                         </video>
-                        <div className="main-img-info-wrapper">
-                        <div className="main-img-info">
-                            <div className="main-img-title-videoEnds">
-                                <img src={require('./img/Netflix-Main-img-title.png')} alt=""/>
-                            </div>
-                            <div className="main-img-subtitle-videoEnds">
-                            </div>
-                            <div className="main-img-info-button-wrapper">
-                                <a href="/">
-                                    <button>
-                                        <FontAwesomeIcon icon={faPlay} className="faPlay" />
-                                        <span>재생</span>
-                                    </button>
-                                </a>
-                                <a href="/">
-                                    <button>
-                                        <FontAwesomeIcon icon={faCircleInfo} className="faCircleInfo"/>
-                                        <span>상세 정보</span>
-                                    </button>
-                                </a>
+                        <div className="modal-main-img-info-wrapper">
+                            <div className="main-img-info">
+                                <div className="main-img-title-videoEnds">
+                                    <img src={require('./img/Netflix-Main-img-title.png')} alt=""/>
+                                </div>
+                                <div className="main-img-subtitle-videoEnds">
+                                </div>
+                                <div className="main-img-info-button-wrapper">
+                                    <a href="/">
+                                        <button>
+                                            <FontAwesomeIcon icon={faPlay} className="faPlay" />
+                                            <span>재생</span>
+                                        </button>
+                                    </a>
+                                    <div id='asd'>
+                                        <FontAwesomeIcon icon={faPlus} className="faCirclePlus" />
+                                    </div>
+                                    <div>
+                                        <FontAwesomeIcon icon={faThumbsUp} className="faThumbsUp" />
+                                    </div>
+                                    <div>
+                                        <FontAwesomeIcon icon={faThumbsDown} className="faThumbsDown" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
                         <div className='preview-modal-close' >
                             <button onClick={close}>close</button>
                         </div>

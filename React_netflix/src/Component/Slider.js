ed3cd5dd6d9fee29ed4ff42d6720ea2852e812a9
이지윤 <1yoouoo@gmail.com>
2022-11-13 19:46:55 +0900

    Design: Slide handler

--- React_netflix/src/Component/Slider.js
+++ React_netflix/src/Component/Slider.js
@@ -1,13 +1,24 @@
 import "./Slider.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
 
 const Slider = (props) => {
     return (
-        <>
+        <>  
             <h3 className="sliderTitle">
                 {props.title}
             </h3>
-            <div className="sliderMask">
-                {props.imgList}
+            <div className="handle-wrapper">
+                <div className="handle handle-left">
+                    <FontAwesomeIcon icon={faChevronLeft} className="faChevronLeft" />
+                </div>
+                <div className="sliderMask">
+                    {props.imgList}
+                </div>
+                <div className="handle handle-right">
+                    <FontAwesomeIcon icon={faChevronRight} className="faChevronRight" />
+                </div>
             </div>
         </>
     )
